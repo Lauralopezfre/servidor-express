be496a0d31343de11780ac37700bef613fe47c66
Laura Edith López Fregoso <laura.lopez204820@potros.itson.edu.mx>
2022-03-09 12:13:50 -0700

    Setting up a web server with Express

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+
+require('dotenv').config();
+console.log(process.env)
+
+//Paso 1: Exponer archivos en una ruta específica.
+app.use('/archivo_prueba',express.static('public')) 
+app.use('/archivo_prueba/imagen1',express.static('public/prueba_covid.jpg')) 
+app.use('/archivo_prueba/imagen2',express.static('public/software_prueba.jpg')) 
+app.use('/archivo_prueba/imagen3',express.static('public/testing.jpg')) 
+
+
+//Paso 2: Responder un texto (el que desee) cuando se le haga un get a la ruta /holamundo
+app.get('/', (req, res)=>{
+    res.send('Hola mundo')
+})
+
+//Paso 3: Tener la capacidad de recibir un texto (en el cuerpo del mensaje) mediante el método POST
+app.post('/', function (req, res) {
+    res.send('Request a text');
+  });
+
+app.put('/classmates', function (req, res) {
+    res.send('Got a PUT request at /classmates');
+});
+
+app.listen(process.env.PORT, ()=>{
+    console.log('Servidor corriendo en el puerto ' + process.env.PORT)
+})
